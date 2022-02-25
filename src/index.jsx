d82a03adce1e74a23d6d388d767f7c752c70a606
Omar Mashaal <omar@teacups.io>
2022-02-25 20:33:14 +1100

    react-18

---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+import React, { lazy, Suspense } from "react";
+import Spinner from "./components/spinner.jsx";
+import Post from "./components/post.jsx";
+import Sidebar from "./components/sidebar.jsx";
+import Comments from "./components/comments.jsx";
+
+const Index = () => {
+  return (
+    <main>
+      <aside className="sidebar">
+        <Suspense fallback={<Spinner />}>
+          <Sidebar />
+        </Suspense>
+      </aside>
+      <article className="post">
+        <Suspense fallback={<Spinner />}>
+          <Post />
+        </Suspense>
+        <section className="comments">
+          <h2>Comments</h2>
+          <Suspense fallback={<Spinner />}>
+            <Comments date={+new Date()} />
+          </Suspense>
+        </section>
+        <h2>Thanks for reading!</h2>
+      </article>
+    </main>
+  );
+};
+
+export default Index;
