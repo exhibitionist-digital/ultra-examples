86f3dff03df8e015aa74770d8a6786b976f3c6c7
Omar Mashaal <omar@teacups.io>
2022-03-11 08:41:55 +1100

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import ultra from "https://deno.land/x/ultra@v0.7.6/mod.ts";
+import ultra from "https://deno.land/x/ultra/mod.ts";
 import importmap from "./importmap.json" assert { type: "json" };
 
 await ultra({
