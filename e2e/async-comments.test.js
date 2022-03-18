389ad9eda8cf50abf5b0ee64e175c7639c8e6db8
Craig Doremus <cdoremus@gmail.com>
2022-03-18 13:10:35 -0400

    e2e-react=18: deno fmt fixes

--- e2e/async-comments.test.js
+++ e2e/async-comments.test.js
@@ -1,42 +1,44 @@
-import puppeteer from 'https://deno.land/x/puppeteer@9.0.2/mod.ts';
+import puppeteer from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
 import {
   assertEquals,
   fail,
 } from "https://deno.land/std@0.130.0/testing/asserts.ts";
 
-Deno.test('Should display all async rendered comments on react-18 example app', async () => {
-    const expected_comments = [
-      "Wait, it doesn't wait for React to load?",
-      "How does this even work?",
-      "I like marshmallows",
-    ];
-    const browser = await puppeteer.launch({
-        executablePath: Deno.env.get('chrome_path'),
-        headless: true,
-      }
-    );
-    const page = await browser.newPage();
-    await page.setViewport({ width: 979, height: 865 });
-    await page.goto('http://localhost:8000/');
+Deno.test("Should display all async rendered comments on react-18 example app", async () => {
+  const expected_comments = [
+    "Wait, it doesn't wait for React to load?",
+    "How does this even work?",
+    "I like marshmallows",
+  ];
+  const browser = await puppeteer.launch({
+    executablePath: Deno.env.get("chrome_path"),
+    headless: true,
+  });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 979, height: 865 });
+  await page.goto("http://localhost:8000/");
 
-    try {
-      const len = expected_comments.length;
-      let index = 2; // nth-child css selector index
+  try {
+    const len = expected_comments.length;
+    let index = 2; // nth-child css selector index
 
-      for (let i = 0; i < len; i++) {
-        const selector = `section > p:nth-child(${index})`;
-        index = index + 1;
-        const comment = await page.waitForSelector(selector, {timeout: 10000});
-        if (comment) {
-          const text = await page.evaluate(element => element.textContent, comment);
-          assertEquals(text, expected_comments[i]);
-        } else {
-          fail(`ERROR: Selector ${selector} not found`);
-        }
+    for (let i = 0; i < len; i++) {
+      const selector = `section > p:nth-child(${index})`;
+      index = index + 1;
+      const comment = await page.waitForSelector(selector, { timeout: 10000 });
+      if (comment) {
+        const text = await page.evaluate(
+          (element) => element.textContent,
+          comment,
+        );
+        assertEquals(text, expected_comments[i]);
+      } else {
+        fail(`ERROR: Selector ${selector} not found`);
       }
-    } catch (e) {
-      fail(`ERROR: ${e}`);
-    } finally {
-      await browser.close();
     }
+  } catch (e) {
+    fail(`ERROR: ${e}`);
+  } finally {
+    await browser.close();
+  }
 });
