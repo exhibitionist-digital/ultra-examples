94211842326113f2d0c3f03534df971a11bf7c18
Omar Mashaal <omar@teacups.io>
2022-03-27 15:00:11 +1100

    test 0.8.0

--- server.ts
+++ server.ts
@@ -1,6 +1,3 @@
-import ultra from "https://deno.land/x/ultra/mod.ts";
-import importmap from "./importmap.json" assert { type: "json" };
+import ultra from "https://3511-2001-8004-5110-1990-1885-c8db-3d08-9c9f.ngrok.io/mod.ts";
 
-await ultra({
-  importmap,
-});
+await ultra();
