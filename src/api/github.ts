8b6a1e9a4f91f091212004c54f301a366f46fdb2
Omar Mashaal <omar@teacups.io>
2022-03-27 16:48:47 +1100

    test

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,13 +1,10 @@
-export default async () => {
+export default () => {
   const headers = {
     "content-type": "application/json",
   };
-  const data = await fetch(
-    `https://api.github.com/repos/exhibitionist-digital/ultra`,
-  );
-  const github = await data.json();
+
   const body = JSON.stringify({
-    stargazers_count: github?.stargazers_count || "GitHub",
+    stargazers_count: "GitHub",
   });
   return new Response(body, { headers });
 };
